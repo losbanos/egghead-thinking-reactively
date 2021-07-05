9927aed4cc2ed00807106756fde11858c5ec35b1
KimTaeHoon <wwwholakr@gmail.com>
2021-07-06 01:01:58 +0900

    Maintain shared observable state using the scan and shareReplay operators

--- src/lesson-code/TaskProgressService.ts
+++ src/lesson-code/TaskProgressService.ts
@@ -1,15 +1,19 @@
-import { Observable, merge } from 'rxjs';
-import {mapTo, scan} from 'rxjs/operators';
+import {Observable, merge} from 'rxjs';
+import {distinctUntilChanged, mapTo, scan, shareReplay, startWith} from 'rxjs/operators';
 
 const taskStart: Observable<any> = new Observable();
 const taskComplete: Observable<any> = new Observable();
 const showSpinner: Observable<any> = new Observable();
 
 const loadVariables: Observable<number> = merge(taskStart.pipe(mapTo(1)), taskComplete.pipe(mapTo(-1)));
-loadVariables.pipe(
+const currentLoadCount$: Observable<number> = loadVariables.pipe(
+    startWith(0),
     scan((totalLoads, changeInLoads) => {
-        return totalLoads + changeInLoads;
-    }, 0)
+        const newLoadCount: number = totalLoads + changeInLoads;
+        return newLoadCount > 0 ? newLoadCount : 0;
+    }),
+    distinctUntilChanged(),
+    shareReplay({bufferSize: 1, refCount: true})
 )
 export default {
 
